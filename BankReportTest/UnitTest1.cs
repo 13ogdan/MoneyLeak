ff8e13af1120e228ee13375cc19f11d8bf0136f4
Bohdan Spilnyi <bohdan.spilnyi@3shape.com>
2018-11-09 17:59:42 +0200

    Add test project and first report

---
+++ BankReportTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BankReportTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
